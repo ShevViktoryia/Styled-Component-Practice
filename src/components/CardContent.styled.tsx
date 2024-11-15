7a92b05487638bb15663fc07c0936c96a0703a01
Victoria Shevchenko <shevviktoryia@gmail.com>
2024-11-15 23:08:19 +0300

    Updates

--- src/components/CardContent.styled.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const CardContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  width: 100%;
-  padding: 20px 10px;
-`;
--- src/components/CardHeader.styled.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from "styled-components";
-
-const CardHeader = styled.h2`
-  font-weight: 700;
-  font-size: 16px;
-  color: #000;
-`;
-
-export default CardHeader;
